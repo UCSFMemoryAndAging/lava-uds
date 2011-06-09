8a53597300770d86142f491fb4e6714c9104f707
Charlie Toohey <ctoohey@memory.ucsf.edu>
2011-06-09 22:08:09 +0000

    emory: turns out we already has the cdrScale lists and they were correctly labeled as form b4 not b7, so got rid of all that stuff

--- lava-crms-nacc/database/crms-nacc/development/data/uds_cdr_lists.sql
+++ lava-crms-nacc/database/crms-nacc/development/data/uds_cdr_lists.sql
@@ -1,20 +1,2 @@
-UPDATE viewproperty SET list = 'uds.b7.cdrScale', 
-listAttributes = 'codes=ref.udsErrorCodesDecimal'
-WHERE entity LIKE 'udscdr%' 
-AND property IN ('memory','orient','judgement','commun','homeHobb','comport','cdrLang');
-
-UPDATE viewproperty SET list = 'uds.b7.cdrScaleNoPoint5', 
-listAttributes = 'codes=ref.udsErrorCodesDecimal'
-WHERE entity LIKE 'udscdr%' 
-AND property IN ('persCare');
-
 UPDATE viewproperty SET list = 'ref.totalErrorCodeDecimal' WHERE entity LIKE 'udscdr%' 
 AND property IN ('cdrSum','cdrGlob');
-
-INSERT INTO list (ListName,NumericKey) VALUES ('CDRScale',1);
-SELECT @listID := ListID FROM list WHERE ListName = 'CDRScale';
-INSERT INTO listvalues (ListID,ValueKey,ValueDesc,OrderID) VALUES(@listID,'0','None',1);
-INSERT INTO listvalues (ListID,ValueKey,ValueDesc,OrderID) VALUES(@listID,'.5','Questionable',2);
-INSERT INTO listvalues (ListID,ValueKey,ValueDesc,OrderID) VALUES(@listID,'1','Mild',3);
-INSERT INTO listvalues (ListID,ValueKey,ValueDesc,OrderID) VALUES(@listID,'2','Moderate',4);
-INSERT INTO listvalues (ListID,ValueKey,ValueDesc,OrderID) VALUES(@listID,'3','Severe',5);
