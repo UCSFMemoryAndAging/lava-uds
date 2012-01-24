87bb9a8c6425ba916cedc5ec2a0fefae376889a7
Joe Hesse <jhesse@memory.ucsf.edu>
2012-01-24 22:02:31 +0000

    Updated for V3.1

--- lava-crms-nacc/database/crms-nacc/development/data/extract-crms-nacc-data.sql
+++ lava-crms-nacc/database/crms-nacc/development/data/extract-crms-nacc-data.sql
@@ -1,6 +1,6 @@
 SELECT 'DELETE from datadictionary where instance=\'lava\' and scope=\'crms-nacc\';';  
 SELECT 'DELETE from viewproperty where instance=\'lava\' and scope=\'crms-nacc\';';
 SELECT 'DELETE from hibernateproperty where instance=\'lava\' and scope=\'crms-nacc\';';  
-SELECT 'DELETE from `listvalues` where `ListID` in (SELECT `ListID` from `list` where `scope`=''crms-nacc'');';
-SELECT 'DELETE from `list` where `scope`=''crms-nacc'';';
+SELECT 'DELETE from `listvalues` where instance=\'lava\' and scope=\'crms-nacc\';';
+SELECT 'DELETE from `list` where instance=\'lava\' and scope=\'crms-nacc\';';
 call util_CreateMetadataInsertStatements('lava','crms-nacc','%');
--- lava-crms-nacc/database/crms-nacc/development/data/uds_cdr_lists.sql
+++
@@ -1,2 +0,0 @@
-UPDATE viewproperty SET list = 'ref.totalErrorCodeDecimal' WHERE entity LIKE 'udscdr%' 
-AND property IN ('cdrSum','cdrGlob');
