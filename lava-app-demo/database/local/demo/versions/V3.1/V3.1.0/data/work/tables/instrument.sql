f3f2f9866eabc78644ddf79f2ab5e202c1ca2868
Charlie Toohey <ctoohey@memory.ucsf.edu>
2012-05-18 22:58:38 +0000

    revert to tag V3-1-0-0_20120127

---
+++ lava-app-demo/database/local/demo/versions/V3.1/V3.1.0/data/work/tables/instrument.sql
@@ -0,0 +1,26 @@
+/*
+-- Query: SELECT * FROM `lava_demo`.`instrument`
+-- Date: 2012-01-25 20:27
+*/
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (24,'NACC Pathology','NACCPathology','NACCPathology',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (79,'UDS Appraisal','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (80,'UDS CDR','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (81,'UDS Diagnosis','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (82,'UDS Family History','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (83,'UDS FAQ','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (84,'UDS GDS','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (85,'UDS Hachinski','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (86,'UDS Health History','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (87,'UDS Informant Demo','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (88,'UDS Labs Imaging','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (89,'UDS Medications','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (90,'UDS NeuroPsych','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (91,'UDS NPI','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (92,'UDS Physical','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (93,'UDS Subject Demo','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (94,'UDS Symptoms Onset','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (95,'UDS UPDRS','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (97,'UDS Milestone','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (105,'MDS Status','MDSStatus','MDSStatus',NULL,0,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (106,'UDS Form Checklist','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
+INSERT INTO `instrument` (InstrID,InstrName,TableName,FormName,Category,HasVersion,modified) VALUES (144,'UDS Phone Inclusion','Multiple','LavaWebOnly',NULL,1,'2009-01-25 04:57:36');
--- lava-app-demo/security/local/examiner/redirect.jsp
+++
@@ -1,1 +0,0 @@
-<jsp:forward page="/crms/home/home/home.lava"/>
