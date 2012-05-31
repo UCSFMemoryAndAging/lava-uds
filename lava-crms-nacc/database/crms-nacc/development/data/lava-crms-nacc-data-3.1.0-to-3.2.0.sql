7ba2941f5a0eff89647d06e46f20b57516cb3018
Charlie Toohey <ctoohey@memory.ucsf.edu>
2012-05-31 23:39:17 +0000

    emory fix:
    I���ve changed the list values for age of onset in both A3 and B9.  They used to be 0-120 but the data dictionary says 15-110.

---
+++ lava-crms-nacc/database/crms-nacc/development/data/lava-crms-nacc-data-3.1.0-to-3.2.0.sql
@@ -0,0 +1,30 @@
+-- change lists from the general age lists that were of range 0-120 (used to be uds.ageOnsetUnknown and uds.ageOnsetNAUnknown), but should \
+be 15-110                                                                                                                                   
+UPDATE viewproperty SET list='uds.ageOnsetNAUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory1' AND propert\
+y='momOnset';
+UPDATE viewproperty SET list='uds.ageOnsetNAUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory1' AND propert\
+y='dadOnset';
+UPDATE viewproperty SET list='uds.ageOnsetNAUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory1' AND propert\
+y LIKE 'sib%Ons';
+UPDATE viewproperty SET list='uds.ageOnsetNAUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory1' AND propert\
+y LIKE 'kids%Ons';
+UPDATE viewproperty SET list='uds.ageOnsetNAUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory1' AND propert\
+y LIKE 'rel%Ons';
+UPDATE viewproperty SET list='uds.ageOnsetUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory2' AND property=\
+'momOnset';
+UPDATE viewproperty SET list='uds.ageOnsetUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory2' AND property=\
+'dadOnset';
+UPDATE viewproperty SET list='uds.ageOnsetUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory2' AND property \
+LIKE 'sib%Ons';
+UPDATE viewproperty SET list='uds.ageOnsetUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory2' AND property \
+LIKE 'kid%Ons';
+UPDATE viewproperty SET list='uds.ageOnsetUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udsfamilyhistory2' AND property \
+LIKE 'rel%Ons';
+-- TODO: technically, for follow-up udssymptomsonset1, the decage cannot be unknown, but seems permissible for now                          
+UPDATE viewproperty SET list='uds.ageOnsetUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udssymptomsonset1' AND property \
+LIKE 'decage';
+UPDATE viewproperty SET list='uds.ageOnsetNAUnknown' WHERE instance='lava' AND scope='crms-nacc' AND entity='udssymptomsonset2' AND propert\
+y LIKE 'decage';
+
+
+
