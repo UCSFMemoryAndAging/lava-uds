5fd2c59adb0d81cc58b8f088f0b655951efcc365
Charlie Toohey <ctoohey@memory.ucsf.edu>
2013-08-13 00:15:44 +0000

    initial revision

--- lava-crms-nacc/database/crms-nacc/versions/V3.3/V3.3.2/update/lava-crms-nacc-data-3.3.1-to-3.3.2.sql
+++ lava-crms-nacc/database/crms-nacc/versions/V3.3/V3.3.2/update/lava-crms-nacc-data-3.3.1-to-3.3.2.sql
@@ -5,6 +5,19 @@ UPDATE viewproperty
   SET attributes='onkeypress="onlyNumsAllowed(event,true,false)"'
   WHERE instance='lava' AND scope='crms-nacc' AND entity like 'udsphysical%' AND property='height';
 
+-- ***********************************************************************************
+-- EMORY: fixes problem with HTML interpretation of list data (some browsers, e.g. IE)
+-- ***********************************************************************************
+-- ValueKey used to be "<=6 months"
+SET @LISTID=(SELECT ListID FROM list WHERE ListName='udsb9mode' AND
+instance='lava' AND scope='crms-nacc');
+UPDATE listvalues SET ValueDesc='Subacute(&lt;=6 months)' WHERE
+ListID=@LISTID AND ValueKey='2';
+
+--- ****************************************************************************************
+--- UCSF fix bug that prevented from entering a drug that was not listed in UDS Medications2
+--- ****************************************************************************************
+UPDATE viewproperty SET attributes = NULL WHERE entity='udsmedications2' AND property='notListed';
 
 
 INSERT INTO versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
