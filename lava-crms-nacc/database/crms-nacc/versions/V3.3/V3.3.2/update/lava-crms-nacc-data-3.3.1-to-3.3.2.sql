8a8b20be84f01f17e6906d55a0fd094ea88815b8
alee7 <alee@memory.ucsf.edu>
2014-02-24 12:58:22 -0800

    added bug fix for missing db columns for udsftldfamilyhistory

--- lava-crms-nacc/database/crms-nacc/versions/V3.3/V3.3.2/update/lava-crms-nacc-data-3.3.1-to-3.3.2.sql
+++ lava-crms-nacc/database/crms-nacc/versions/V3.3/V3.3.2/update/lava-crms-nacc-data-3.3.1-to-3.3.2.sql
@@ -19,9 +19,15 @@ ListID=@LISTID AND ValueKey='2';
 -- ****************************************************************************************
 UPDATE viewproperty SET attributes = NULL WHERE entity='udsmedications2' AND property='notListed';
 
+-- *********************************************************************************
+-- UCSF fix bug for UDS Medications A4 form where columns were missing from db table
+-- *********************************************************************************
+ALTER TABLE `udsftldfamilyhistory` ADD COLUMN `FTDFA3F1` SMALLINT NULL DEFAULT NULL AFTER `InstrID` ;
+ALTER TABLE `udsftldfamilyhistory` ADD COLUMN `FTDFA3F2` SMALLINT NULL DEFAULT NULL AFTER `FTDMOTHX` ;
+ALTER TABLE `udsftldfamilyhistory` ADD COLUMN `FTDFA3F3` SMALLINT NULL DEFAULT NULL AFTER `FTDDADA` ;
+ALTER TABLE `udsftldfamilyhistory` ADD COLUMN `FTDFA3F4` SMALLINT NULL DEFAULT NULL AFTER `FTDASM` ;
+UPDATE `viewproperty` set label='' where entity='udsftldfamilyhistory2';
 
 INSERT INTO versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
 	VALUES ('lava-crms-nacc-model','3.3.2',NOW(),3,3,2,0);
 
-
- 
