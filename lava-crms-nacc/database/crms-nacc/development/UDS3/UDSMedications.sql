b8f0a84a03f5e3d4f0bf3bb00b2d65319cb3f546
ctoohey <ctoohey@memory.ucsf.edu>
2015-02-25 12:36:47 -0800

    initial revision

---
+++ lava-crms-nacc/database/crms-nacc/development/UDS3/UDSMedications.sql
@@ -0,0 +1,5 @@
+-- no table changes
+
+-- no metadata needed because all UDS 2 metadata can be used as there are no form changes
+-- and the custom UdsMedications2Handler loads the list data for udsmedications2 metadata 
+-- properties
